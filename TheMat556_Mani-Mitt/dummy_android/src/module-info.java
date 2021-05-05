3e3410274e1a1924a7d04dc1251ef56a1d56881a
Marcel Krallinger <s2010824017>
2021-05-05 17:08:31 +0200

    Ultimate insane fix to really sad Problem!

--- TheMat556_Mani-Mitt/dummy_android/src/module-info.java
+++ TheMat556_Mani-Mitt/dummy_android/src/module-info.java
@@ -5,9 +5,9 @@ void thisIsANewMethod(){
 }
 
 module dummy_android {
-int y = 5;
-int x = 6;
-int z = 7;
+int y = 69;
+int x = 420;
+int z = 42;
 java.lang.System.out.println( x );
 java.lang.System.out.println( y );
 java.lang.System.out.println( z );
