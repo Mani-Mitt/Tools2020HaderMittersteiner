1bdf647f1e20d01ac991ee9c0fc277b9f27d4daa
TheMat556 <79401695+TheMat556@users.noreply.github.com>
2021-05-05 15:33:41 +0200

    bugfix-branch

--- TheMat556_Mani-Mitt/dummy_android/src/module-info.java
+++ TheMat556_Mani-Mitt/dummy_android/src/module-info.java
@@ -3,6 +3,7 @@ void thisIsANewMethod(){java.lang.System.out.print(2)}
 module dummy_android {
 int y = 5;
 int x = 6;
+int z = 7;
 java.lang.System.out.println( x );
 java.lang.System.out.println( y );
 }
